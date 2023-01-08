5cf9a7c3d92a1d01ac377458c22a89b35dd66dc3
gregsleg <gregorjery@gmail.com>
2023-01-08 10:02:20 +0000

    zone sconner

--- Scripts/ENDZONE.gd
+++ Scripts/ENDZONE.gd
@@ -14,7 +14,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
+func nextScene():
+	global.crouching = false
+	global.hidden = false
+	global.bushcount = 0
+	global.spotted = false
+	global.playerhead = Vector3()
+	global.catch_pos = Vector3()
+	global.bar = 0
+	global.seenby = 0
+	if get_tree().get_current_scene().get_name() == "Level1":
+		get_tree().change_scene("res://Scenes/AfterLevel1.tscn")
+	elif get_tree().get_current_scene().get_name() == "AfterLevel1.tscn":
+		get_tree().change_scene("res://Scenes/Level2.tscn")
+	elif get_tree().get_current_scene().get_name() == "Level2.tscn":
+		get_tree().change_scene("res://Scenes/AfterLevel2.tscn")
 
 func _on_ENDZONE_area_entered(area):
 	if area.is_in_group("player"):
@@ -22,3 +36,6 @@ func _on_ENDZONE_area_entered(area):
 			print("Level Complete")
 		else:
 			print("Not Enough Weet")
+	nextScene()
+			
+			
