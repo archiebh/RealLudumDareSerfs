4632653076f4285c3688ef81993a22edbd8ea12e
gregsleg <gregorjery@gmail.com>
2023-01-09 14:51:11 +0000

    finalscene

---
+++ Scripts/guy_stationary_animate.gd
@@ -0,0 +1,19 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var animplayer = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animplayer.set_current_animation("Idle")
+	animplayer.play()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
